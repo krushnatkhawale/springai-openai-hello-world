38e1eda75a1b33877eb66ccfff6867af5b108bb7
krushnat <krushnatkhawale@gmail.com>
2024-01-16 09:44:12 +0530

    Tidy up

--- src/main/java/com/practice/AIGreetingController.java
+++ src/main/java/com/practice/AIGreetingController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AirGreetingController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AirGreetingController.class);
+public class AIGreetingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AIGreetingController.class);
     private final ChatClient chatClient;
-    public AirGreetingController(ChatClient chatClient) {
+    public AIGreetingController(ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
@@ -19,12 +19,14 @@ public class AirGreetingController {
         LOGGER.info("Greeting request received");
         final String aiClientResponse = chatClient.generate(
                 """
-                As a modern generative AI model,
-                Generate a 5 liner greeting message in your style for a human in text form.
-                Thanks in advance.     
+                    As a modern generative AI model,
+                    Generate a 5 liner greeting message in your style for a human in text form.
+                    Thanks in advance.
                 """
         );
         LOGGER.info("AI Response: {}", aiClientResponse);
         return aiClientResponse;
     }
+
+
 }
